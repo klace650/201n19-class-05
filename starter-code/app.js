24496bba27308e7a1493112b69055d1bec51297a
Klace Koch <koch.klace@gmail.com>
2020-08-01 15:55:19 -0700

    Sum and Multiply Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,10 +9,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  var theSum = a + b;
-  var theString = `The sum of ${a} and ${b} is ${theSum}.`;
-  // console.log(theSum, theString);
-  return [theSum, theString];
+  var sumOf = a + b;
+  var sumString = `The sum of ${a} and ${b} is ${sumOf}.`;
+  // console.log(sumOf, sumString);
+  return [sumOf, sumString];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -32,7 +32,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   var productOf = a * b;
   var productString = `The product of ${a} and ${b} is ${productOf}.`;
-  console.log(productString);
+  // console.log(productString);
   return [productOf, productString];
 }
 
@@ -54,11 +54,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumOfAll = sum(sum(a,b)[0],c)[0];
+    // console.log(sumOfAll)
+  var productOfAll = multiply(multiply(a,b)[0],c)[0]; //Fuzzy on why it needs square brace
+    // console.log(productOfAll);
+  var allSumString = `${a} and ${b} and ${c} sum to ${sumOfAll}.`
+    // console.log(allSumString)
+  var allProductString = `The product of ${a} and ${b} and ${c} is ${productOfAll}.`
+    // console.log(allProductString)
+  return [sumOfAll, productOfAll, allSumString, allProductString]
 }
-
+// sumAndMultiply(4,7,5)
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
